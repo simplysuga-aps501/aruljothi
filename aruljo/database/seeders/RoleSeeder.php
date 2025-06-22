d67d3fb844e94cb5bffd01ba1e1a27833d50d9a9
Sugaprakash <simplysuga_aps501@avvaiyarpadasalai.org>
2025-06-22 08:36:49 -0400

    20250622 simplysuga Spatie with aruljo roles

---
+++ aruljo/database/seeders/RoleSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Spatie\Permission\Models\Role;
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //volunteer, parent, student
+        Role::create(['name' => 'admin']); 
+        Role::create(['name' => 'owner']);
+        Role::create(['name' => 'staff']);
+        Role::create(['name' => 'euser']);
+    }
+}
+
